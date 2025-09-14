88c6bc4adf9ee8f8690e0d6b8daf95311229b2aa
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-09-14 16:35:02 -0300

    Endpoints Task: insert end update

--- backend/src/main/java/com/practicing/to_do_list/resource/TaskResource.java
+++ backend/src/main/java/com/practicing/to_do_list/resource/TaskResource.java
@@ -1,14 +1,20 @@
 package com.practicing.to_do_list.resource;
 
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.practicing.to_do_list.dto.TaskDTO;
 import com.practicing.to_do_list.services.TaskService;
@@ -29,6 +35,19 @@ public class TaskResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<TaskDTO> findById(@PathVariable Long id){
 		TaskDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<TaskDTO> insert(@RequestBody TaskDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<TaskDTO> update(@PathVariable Long id, @RequestBody TaskDTO dto){
+		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
 
--- backend/src/main/java/com/practicing/to_do_list/services/TaskService.java
+++ backend/src/main/java/com/practicing/to_do_list/services/TaskService.java
@@ -8,10 +8,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.practicing.to_do_list.dto.CategoryDTO;
 import com.practicing.to_do_list.dto.TaskDTO;
+import com.practicing.to_do_list.entities.Category;
 import com.practicing.to_do_list.entities.Task;
+import com.practicing.to_do_list.repositories.CategoryRepository;
 import com.practicing.to_do_list.repositories.TaskRepository;
 import com.practicing.to_do_list.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 
 
@@ -20,6 +25,9 @@ public class TaskService {
 	
 	@Autowired
 	private  TaskRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
 	public Page<TaskDTO> findAllPaged(Pageable pageable){
@@ -33,6 +41,38 @@ public class TaskService {
 		Optional<Task> result = repository.findById(id);
 		Task entity = result.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new TaskDTO(entity, entity.getCategories());
+	}
+	
+	@Transactional
+	public TaskDTO insert(TaskDTO dto) {
+		Task entity = new Task();
+		copyDTOToEntoty(dto, entity);
+		entity = repository.save(entity);
+		return new TaskDTO(entity);
+	}
+	
+	@Transactional
+	public TaskDTO update(Long id, TaskDTO dto) {
+		try {
+		Task entity = repository.getReferenceById(id);
+		copyDTOToEntoty(dto, entity);
+		entity = repository.save(entity);
+		return new TaskDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	private void copyDTOToEntoty(TaskDTO dto, Task entity) {
+		entity.setTitle(dto.getTitle());
+		entity.setDescription(dto.getDescription());
+		entity.setStatus(dto.getStatus());
+		
+		entity.getCategories().clear();
+		for(CategoryDTO taskDto : dto.getCategories()) {
+			Category category = categoryRepository.getReferenceById(taskDto.getId());
+			entity.getCategories().add(category);
+		}		
 	}
 
 }
